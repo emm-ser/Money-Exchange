87d1a12b8e6fc13fab912eae9b9e7cc8cb7988e8
emm-ser <106416@bk.ru>
2018-02-28 08:30:14 +0200

    Money-Exchange/feature: solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  if (currency > 10000) {
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  } else if (currency <= 0) {
+    return {};
+  }
+  
+  var h = Math.floor(currency / 50);
+  currency  = currency - h * 50;
+  var q = Math.floor(currency / 25);
+  currency = currency - q * 25;
+  var d = Math.floor(currency / 10);
+  currency = currency - d * 10;
+  var n = Math.floor(currency / 5);
+  currency = currency - n * 5;
+  var p = currency;
+
+  var obj = {"H":h,"Q":q,"D":d,"N":n,"P":p};
+  if (!h) delete obj.H;
+  if (!q) delete obj.Q;
+  if (!d) delete obj.D;
+  if (!n) delete obj.N;
+  if (!p) delete obj.P;
+
+  return obj;
 }
